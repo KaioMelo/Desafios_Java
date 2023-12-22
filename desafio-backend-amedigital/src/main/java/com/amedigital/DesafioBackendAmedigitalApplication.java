41c68ca38c8a21d8301107c8322329a08fbedde8
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-12-21 21:00:38 -0300

    Subindo atualização de Header e background

---
+++ desafio-backend-amedigital/src/main/java/com/amedigital/DesafioBackendAmedigitalApplication.java
@@ -0,0 +1,13 @@
+package com.amedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendAmedigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendAmedigitalApplication.class, args);
+	}
+
+}
